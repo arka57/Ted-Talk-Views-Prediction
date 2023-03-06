977e158b2de6ddbe6224f2e403583f5c4eeac621
arka57 <36561428+arka57@users.noreply.github.com>
2023-03-06 11:06:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request
 import pickle
 import numpy as np
+import pandas as pd
 
 model=pickle.load(open('model.pkl','rb'))
 model1=pickle.load(open('model1.pkl','rb'))
@@ -14,18 +15,28 @@ def index():
 def predict_ted_talk_views():
     c=float(request.form.get('comments'))
     d=float(request.form.get('duration'))
-    days_old=int(request.form.get('days_old'))
-    is_weekend=int(request.form.get('is_weekend'))
+    p_date=request.form.get('p_date')
+    r_date=request.form.get('r_date')
+
+    recorded_date=pd.to_datetime(r_date)
+    published_date=pd.to_datetime(p_date)
+    days_old=(published_date-recorded_date).days
+    is_weekend=published_date.dayofweek
+    #converting weekend-1 and otherwise-0
+    if is_weekend >=5:
+        is_weekend=1
+    else:
+        is_weekend=0    
     alc=int(request.form.get('alc'))
 
     #prediction
     x=np.array([c,d,days_old,is_weekend,alc]).reshape(1,5)
     y=model1.transform(x)
     result=model.predict(y)
-    result1=np.exp(result)  
+    result1=np.exp(result) 
 
     return render_template('index.html',result=result1)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    app.run(debug=True)
